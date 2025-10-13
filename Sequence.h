0b71ff6c4b0ba1ed7d6158a3d0a86c2756ce687a
Adam Hume <ahume2000@gmail.com>
2025-10-12 21:36:15 -0400

    Template for header started with the bare minimum!

---
+++ Sequence.h
@@ -0,0 +1,19 @@
+/*
+/ Adam Hume
+/ Project 3 - Linked Sequence Data Structure
+/
+/ This is the header file. It holds all the declarations for
+/ functions and variables both public and private for the Sequence class
+*/
+
+#ifndef PROJECT3_SEQUENCE_H
+#define PROJECT3_SEQUENCE_H
+
+class Sequence {
+    private:
+
+    public:
+    Sequence();
+};
+
+#endif //PROJECT3_SEQUENCE_H
