c7f684409d3b7765c75c93a4f3e4360d3406777c
Adam Hume <ahume2000@gmail.com>
2025-10-12 22:46:28 -0400

    Increased readability by using inline comments for variables and leading (block) comments for functions

--- Sequence.h
+++ Sequence.h
@@ -11,15 +11,14 @@
 #include <string>
 
 class SequenceNode {
-public: // to make it easier, we can make the data members public so we don't need
-    // getters and setters
-    SequenceNode* next; // pointer to next Node. If node is the tail, next is
-    // nullptr
-    SequenceNode* prev; // pointer to previous Node. If node is the head, prev is
-    // nullptr
-    std::string item; // the element being stored in the node
-    //default constructor, ensure next and prev are nullptr
+public: // to make it easier, we can make the data members public so we don't need getters and setters
+    SequenceNode* next; // pointer to next Node. If node is the tail, next is nullptr
+    SequenceNode* prev; // pointer to previous Node. If node is the head, prev is nullptr
+    std::string item; // the element being stored in the node default constructor, ensure next and prev are nullptr
+
+    // default class constructor set to populate next and prev with null pointers
     SequenceNode() : next(nullptr), prev(nullptr) {}
+
     /// parameterized constructor, next and prev are set to nullptr and the
     /// node's element is set to the given value
     SequenceNode(std::string item) : next(nullptr), prev(nullptr), item(item) {}
@@ -32,46 +31,34 @@ class Sequence {
         size_t sequenceSize; // Number of elements in the sequence
 
     public:
-        // Creates an empty sequence (numElts == 0) or a sequence of numElts items
-        // indexed from 0 ... (numElts - 1).
+        // Creates an empty sequence or a sequence with "sz" number of nullptrs
         Sequence(size_t sz = 0);
 
         // Creates a (deep) copy of sequence s
         Sequence(const Sequence& s);
 
-        // Destroys all items in the sequence and release the memory
-        // associated with the sequence
+        // Destroys all items in sequence and release all memory associated
         ~Sequence();
 
-        // The current sequence is released and replaced by a (deep) copy of sequence
-        // s. A reference to the copied sequence is returned (return *this;).
+        // replace calling sequence with a deep copy of the other
         Sequence& operator=(const Sequence& s);
 
-        // The position satisfies ( position >= 0 && position <= last_index() ).
-        // The return value is a reference to the item at index position in the
-        // sequence. Throws an exception if the position is outside the bounds
-        // of the sequence
+        // overload "[]" operator to work with sequence
         std::string& operator[](size_t position);
 
-        // The value of item is appended to the sequence.
+        // push value onto the end of the sequence.
         void push_back(std::string item);
 
-        // the item at the end of the sequence is deleted and size of the sequence is
-        // reduced by one. If sequence was empty, throws an exception
+        // delete last element of sequence (element at tail)
         void pop_back();
 
-        // The position satisfies ( position >= 0 && position <= last_index() ). The
-        // value of item is inserted at position and the size of sequence is increased
-        // by one. Throws an exceptionif the position is outside the bounds of the
-        // sequence
+        // insert element into sequence at position.
         void insert(size_t position, std::string item);
 
-        // Returns the first element in the sequence. If the sequence is empty, throw an
-        // exception.
+        // Get first element in the sequence.
         std::string front() const;
 
-        // Return the last element in the sequence. If the sequence is empty, throw an
-        // exception.
+        // Get last element in the sequence.
         std::string back() const;
 
         // Return true if the sequence has no elements, otherwise false.
@@ -80,23 +67,16 @@ class Sequence {
         // Return the number of elements in the sequence.
         size_t size() const;
 
-        // All items in the sequence are deleted and the memory associated with the
-        // sequence is released, resetting the sequence to an empty state that can have
-        // items re-inserted.
-        void clear();
+        // free all memory (of elements) and remove from sequence
+        void clear();// free all memory (of elements) and remove from sequence
 
-        // The item at position is removed from the sequence, and the memory
-        // is released. If called with an invalid position throws an exception.
+        // free memory and remove element at postion.
         void erase(size_t position);
 
-        // The items in the sequence at ( position ... (position + count - 1) ) are
-        // deleted and their memory released. If called with invalid position and/or
-        // count throws an exception.
+        // free memory and remove "count" number of elements at position.
         void erase(size_t position, size_t count);
 
-        // Outputs all elements (ex: <4, 8, 15, 16, 23, 42>) as a string to the output
-        // stream. This is *not* a method of the Sequence class, but instead it is a
-        // friend function
+        // allow elements to be output to stream by <<
         friend std::ostream& operator<<(std::ostream& os, const Sequence& s);
 
 };
