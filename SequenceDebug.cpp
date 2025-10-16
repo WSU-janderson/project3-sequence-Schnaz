cbccda90178dbb5d23046a0f372539d376a2c181
Adam Hume <ahume2000@gmail.com>
2025-10-16 00:27:50 -0400

    Final reformatting and commenting of Class file. Final test in the works

--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -64,6 +64,70 @@ int main() {
 
     cout << "Front: " + seq1.front() << endl << "Back: " + seq1.back() << endl << endl;
 
+
+    cout << "Test front exception\n";
+    try {
+        cout << seq2.front();
+    }
+    catch (exception &e) {
+        cout << e.what() << endl;
+        cout << "exception thrown" << endl << endl;
+    }
+
+    cout << "Test back exception\n";
+    try {
+        cout << seq2.back();
+    }
+    catch (exception &e) {
+        cout << e.what() << endl;
+        cout << "exception thrown" << endl << endl;
+    }
+
+    cout << "Does empty work?\n";
+    if (seq2.empty()) cout << "seq 2 is empty!\n";
+    if (!seq1.empty()) cout << "seq 1 is full!\n\n";
+
+    cout << "Copy seq 1 to seq 2!\n";
+    seq2 = seq1;
+    cout << seq1.size() << " nodes" << endl << seq2 << endl << endl;
+
+    cout << "Was the copy deep? subtract values and compare Sequences\n";
+    seq2.erase(0);
+    seq2.erase(2,2);
+    cout << seq1.size() << " nodes" << endl << seq1 << endl;
+    cout << seq2.size() << " nodes" << endl << seq2 << endl << endl;
+
+    cout << "Test erase exception 1\n";
+    try {
+        seq2.erase(2);
+    }
+    catch (exception &e) {
+        cout << e.what() << endl;
+        cout << "exception thrown" << endl << endl;
+    }
+
+    cout << "Test erase exception 2\n";
+    try {
+        seq1.erase(0, 6);
+    }
+    catch (exception &e) {
+        cout << e.what() << endl;
+        cout << "exception thrown" << endl << endl;
+    }
+
+    Sequence seq3(seq1);
+    cout << "test copy constructor\n";
+    seq3.erase(4);
+    seq3.erase(1,2);
+    cout << seq1.size() << " nodes" << endl << seq1 << endl;
+    cout << seq3.size() << " nodes" << endl << seq3 << endl << endl;
+
+    //memory leak test. Deconstructor calls clear which calls delete.
+    for (int i = 1000; i > 0; i--) {
+        Sequence *bad = new Sequence(100);
+        delete bad;
+    }
+
     cout << "Your project is ready to go!" << endl;
     return 0;
 }
